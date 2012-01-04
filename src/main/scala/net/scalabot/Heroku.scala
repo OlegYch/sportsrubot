dd25ba49f8235d7dfc913e39bce1dad4f60c80d3
OlegYch <OlegYch@.(none)>
2012-01-04 05:12:53 +0300

    deploying to heroku

--- src/main/scala/net/scalabot/Heroku.scala
+++ src/main/scala/net/scalabot/Heroku.scala
@@ -6,7 +6,7 @@ import java.net.{InetSocketAddress, ServerSocket}
 object Heroku {
   def init(port: Option[String] = Properties.envOrNone("PORT")) {
     port.foreach {port =>
-      new ServerSocket().bind(new InetSocketAddress("localhost", port.toInt))
+      new ServerSocket().bind(new InetSocketAddress("0.0.0.0", port.toInt))
     }
   }
 
